f08b7a8704b415a56f88cbab745cf4973f3a7cbe
unknown <nahuelcittadino@gmail.com>
2023-08-04 15:37:37 -0300

    clean all the coments and console.log

--- twoNumberSum42iChallenge.js
+++ twoNumberSum42iChallenge.js
@@ -1,28 +1,12 @@
 function findPairWithSum(numbers, targetSum) {
-    const numberToShow = {}  //  aqui voy a guardar los numeros que se observaron del array numbers
-    for (let i=0 ; i < numbers.length; i++) { // recorro el array de numeros
-        const num = numbers[i] // guardo el numero actual del array numbers, para poder operar acontinuacion
-        const add = targetSum - num // calculo el complemento que hace falta para alcanzar targetSum
-        //en este condicional if en el primer ciclo del for no se va a cumplir nunca la condicion ya que el bojeto esta vacio
-        if (numberToShow[add] !== undefined) { //verifico que el numero que me falta para alcanzar targetSum esta en el objeto 
-            return `The pair of numbers that sum to ${targetSum} is: ${[num, add]}` //si encontramos el complemento en el objeto numberToShow, retornamos el par de numeros buscados
+    const numberToShow = {}  
+    for (let i=0 ; i < numbers.length; i++) { 
+        const num = numbers[i] 
+        const add = targetSum - num 
+        if (numberToShow[add] !== undefined) { 
+            return `The pair of numbers that sum to ${targetSum} is: ${[num, add]}` 
         }
-        console.log([num,add])
-
-        numberToShow[num] = true; //si la busqueda fracasa y no se encuentra el complemento, se guarda el num en el objeto con una propiedad true para llevar un registro
-        console.log([num,add])
+        numberToShow[num] = true; 
     }
-    return []  // si el ciclo for no encuentra coincidencias se retorna un arrayvacio
+    return []  
 }
-
-const numbers = [3, 6, 8, 10];
-const targetSum = 15;
-findPairWithSum(numbers, targetSum);
-
-/* const numbers = [2, 7, 11, 15];
-const targetSum = 9;
-findPairWithSum(numbers, targetSum); // Output: [2, 7]
-
-const numbers = [3, 6, 8, 10];
-const targetSum = 15;
-findPairWithSum(numbers, targetSum); // Output: [] */
