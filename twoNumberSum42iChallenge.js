1175ea33d86d5eb77e058aa8754fb06d4f36e1d9
unknown <nahuelcittadino@gmail.com>
2023-08-04 15:07:12 -0300

    I successfully traverse the array of numbers

--- twoNumberSum42iChallenge.js
+++ twoNumberSum42iChallenge.js
@@ -1,1 +1,23 @@
-// first commit!
+function findPairWithSum(numbers, targetSum) {
+    const numberToShow = {}  //  aqui voy a guardar los numeros que se observaron del array numbers
+    for (let i=0 ; i < numbers.length; i++) { // recorro el array de numeros
+        const num = numbers[i] // guardo el numero actual del array numbers, para poder operar acontinuacion
+        const add = targetSum - num // calculo el complemento que hace falta para alcanzar targetSum
+
+        console.log(num)
+        console.log(add)
+
+    }
+}
+
+const numbers = [2, 7, 11, 15];
+const targetSum = 9;
+findPairWithSum(numbers, targetSum);
+
+/* const numbers = [2, 7, 11, 15];
+const targetSum = 9;
+findPairWithSum(numbers, targetSum); // Output: [2, 7]
+
+const numbers = [3, 6, 8, 10];
+const targetSum = 15;
+findPairWithSum(numbers, targetSum); // Output: [] */
